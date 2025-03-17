ab361594b4ae791969167dc30bd7fc2bf879ac8d
dummyrock <dfrajerman@gmail.com>
2025-03-17 09:52:05 -0400

    created dfrajerman branch

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Dylan Frajerman
         </p>
         <a
           className="App-link"
