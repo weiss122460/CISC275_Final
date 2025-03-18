e96af24fb3063fe8b2998bd27632bb94b8e28767
Ryan <weiss4163@gmail.com>
2025-03-18 17:27:00 -0400

    Added name

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+           <code>Ryan Weiss</code>
         </p>
         <a
           className="App-link"
