22e7c3cd508c44e369be1b234fcc4356c4c04fc0
Ryan <weiss4163@gmail.com>
2025-03-18 17:28:01 -0400

    fixed formatting

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <code>Ryan Weiss</code>
+           <code>RWeiss</code>
         </p>
         <a
           className="App-link"
